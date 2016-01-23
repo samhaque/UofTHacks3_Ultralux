93dc262b79609b06245a4743e2bbf71b068ae787
jmicaela <jmicaela8@gmail.com>
2016-01-23 07:52:55 -0500

    made layout changes

---
+++ myclass/src/main/java/com/example/Main.java
@@ -0,0 +1,13 @@
+package com.example;
+
+/**
+ * Created by micaela on 2016-01-23.
+ */
+public class Main {
+    public static void main(String[] Args) {
+
+        MyClass j = new MyClass();
+
+        j.decompressMessage("101010110100");
+    }
+}
